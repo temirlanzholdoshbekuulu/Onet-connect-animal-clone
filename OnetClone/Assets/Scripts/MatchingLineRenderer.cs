556de6d5dc582831b0d90f0bf2237758c9a1186e
Temirlan Zholdoshbek uulu <131275795+temirlanzholdoshbekuulu@users.noreply.github.com>
2024-01-22 14:53:07 +0600

    Update MatchingLineRenderer.cs

--- OnetClone/Assets/Scripts/MatchingLineRenderer.cs
+++ OnetClone/Assets/Scripts/MatchingLineRenderer.cs
@@ -14,22 +14,11 @@ public class MatchingLineRenderer : MonoBehaviour
 		lineRenderer = GetComponent<LineRenderer>();
 		Destroy(gameObject, 0.3f);
 
-		bool isHorizontal = startPoint.x != endPoint.x;
-		float distance = isHorizontal ? Mathf.Abs(startPoint.x - endPoint.x) : Mathf.Abs(startPoint.y - endPoint.y);
-		int posCount = Random.Range((int)distance * 5, (int)distance * 8);
-		lineRenderer.positionCount = posCount;
-
 		Vector3 direction = (endPoint - startPoint).normalized;
-		float step = distance / posCount;
 
-		for (int i = 1; i < posCount - 1; i++)
-		{
-			Vector3 jitter = new Vector3(Random.Range(-jitterAmount, jitterAmount), Random.Range(-jitterAmount, jitterAmount), 0);
-			Vector3 position = startPoint + direction * step * i + jitter;
-			lineRenderer.SetPosition(i, position);
-		}
+		lineRenderer.positionCount = 2;
 
 		lineRenderer.SetPosition(0, startPoint);
-		lineRenderer.SetPosition(posCount - 1, endPoint);
+		lineRenderer.SetPosition(1, endPoint);
 	}
 }
