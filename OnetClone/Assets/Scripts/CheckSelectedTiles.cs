0708ba508ef267410e63618020be0912427d1719
Temirlan Zholdoshbek uulu <131275795+temirlanzholdoshbekuulu@users.noreply.github.com>
2024-01-25 15:02:46 +0600

    Small changes in CheckSelectedTiles.cs

--- OnetClone/Assets/Scripts/CheckSelectedTiles.cs
+++ OnetClone/Assets/Scripts/CheckSelectedTiles.cs
@@ -26,30 +26,24 @@ public class CheckSelectedTiles : MonoBehaviour
 	}
 	bool Evaluation1(Tile firstTile, Tile secondTile, List<Tile> EmptyTiles, Vector3 secondTilePos)
 	{
-		if (CheckDirection1(firstTile, secondTile, EmptyTiles, 1, 0)) return true;  // right
-		if (CheckDirection1(firstTile, secondTile, EmptyTiles, -1, 0)) return true; // left
-		if (CheckDirection1(firstTile, secondTile, EmptyTiles, 0, 1)) return true;  // up
-		if (CheckDirection1(firstTile, secondTile, EmptyTiles, 0, -1)) return true; // down
-
-		return false;
+		return 	CheckDirection1(firstTile, secondTile, EmptyTiles, 1, 0) ||
+				CheckDirection1(firstTile, secondTile, EmptyTiles, -1, 0)||
+				CheckDirection1(firstTile, secondTile, EmptyTiles, 0, 1) ||
+				CheckDirection1(firstTile, secondTile, EmptyTiles, 0, -1);
 	}
 	bool Evaluation2(Tile firstTile, Tile secondTile, List<Tile> firstList, List<Tile> EmptyTiles, Vector3 firstTilePos)
 	{
-		if (CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, 1, 0)) return true;  // right
-		if (CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, -1, 0)) return true; // left
-		if (CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, 0, 1)) return true;  // up
-		if (CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, 0, -1)) return true; // down
-
-		return false;
+		return 	CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, 1, 0) ||
+				CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, -1, 0)||
+				CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, 0, 1)||
+				CheckDirection2(firstTile, secondTile, firstList, EmptyTiles, 0, -1);
 	}
 	bool Evaluation3(List<Tile> firstList, List<Tile> secondList, Vector3 firstTilePos, Vector3 secondTilePos)
 	{
-		if (CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, 1, 0)) return true;  // right
-		if (CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, -1, 0)) return true; // left
-		if (CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, 0, 1)) return true;  // up
-		if (CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, 0, -1)) return true; // down
-
-		return false;
+		return 	CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, 1, 0)||
+				CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, -1, 0)||
+				CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, 0, 1)||
+				CheckDirection3(firstList, secondList, firstTilePos, secondTilePos, 0, -1);
 	}
 	bool CheckDirection1(Tile firstTile, Tile secondTile, List<Tile> EmptyTiles, int dx, int dy)
 	{
